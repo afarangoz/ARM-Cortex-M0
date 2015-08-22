46df84212c64110c5860161a376599edeb2a0f9b
afarangoz <afarangoz@hotmail.com>
2015-08-21 19:54:56 -0500

    Agregamos la libreria ALU

---
+++ micro/ALU.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ALU.h"
+unsigned float ADD(unsigned long int R1, unsigned long int R2)
+{
+    return (R1+R2);
+}
+unsigned float SUM(unsigned long int R1, unsigned long int R2);
+{
+    return(R1-R2);
+}
+unsigned float BIC(unsigned long int R1, unsigned long int R2);
+{
+    return(R1&R2);
+}
---
+++ micro/ALU.h
@@ -0,0 +1,23 @@
+/**
+* \brief funcion que suma
+* \param R1 Registro1
+* \param R2 Registro2
+* \return El resultado de la suma entre los dos Registros de entrada
+*/
+unsigned float ADD(unsigned long int R1, unsigned long int R2);
+/**
+* \brief funcion que resta
+* \param R1 Registro1
+* \param R2 Registro2
+* \return El resultado del a resta entre los dos Registros de entrada
+*/
+unsigned float SUM(unsigned long int R1, unsigned long int R2);
+/**
+* \brief funcion AND
+* \param R1 Registro1
+* \param R2 Registro2
+* \return El resultado es la eperacion AND a nivel de bits
+*/
+
+unsigned float BIC(unsigned long int R1, unsigned long int R2);
+
