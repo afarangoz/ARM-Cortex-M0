362669e1a66ebfcf1c421d7d87ddb20e2a0b052f
afarangoz <afarangoz@hotmail.com>
2015-08-27 13:00:52 -0500

    No se Modifico

--- micro/main.c
+++ micro/main.c
@@ -5,12 +5,18 @@
 
 int main()
 {
+    unsigned int banderas[4]={0,0,0,0}; //estas son las banderas del microprocesador las pisiciones indican la bandera
+                                        //[0] es N es 1 si el resultado es negatico
+                                        //[1] es Z es 1 si el resultado es cero
+                                        //[2] es C es 1 si hay acarreo en la operacion aritmetica
+                                        //[3] es U es 1 si hubo sobreflujo es la operacion
+
     unsigned long int Reg[13];
 
 
-    Reg[1]=2;
-    Reg[2]=4;
-    Reg[3]=MVN(Reg[1]);
+    Reg[1]=-2;
+    Reg[2]=2;
+    ADD(&Reg[3],Reg[1],Reg[2],&banderas[0]);
     mostrar_registros(Reg);
 
 
--- micro/registros.h
+++ micro/registros.h
@@ -1,1 +1,10 @@
+//<<<<<<< HEAD
+/**
+* \brief Funcion que toma los valores de los registros y los imprime en pantalla
+* \param registro[13] Arreglo que contiene cada 1 de los registros
+* \return No tiene retorno
+*/
+
+//=======
 void mostrar_registros(unsigned long registro[13]);
+//>>>>>>> bd69f40820c1faee386bd8dc906150df923ae255
