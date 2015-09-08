bfa629f5048349224736e74a3a72c6abbad0d6c4
afarangoz <afarangoz@hotmail.com>
2015-09-08 11:22:58 -0500

    Se creo el archivo test.c, se modifico la funcion mostrar banderas y se dejo la funcion ADD para prueba

---
+++ micro/test.c
@@ -0,0 +1,43 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include<stdint.h>
+#include "registros.h"
+#include "ALU.h"
+#include <math.h>
+#include <curses.h>
+
+int main()
+{
+    uint32_t Reg[13]={0,0,0,0,0,0,0,0,0,0,0};
+    uint32_t banderas[4]={0,0,0,0}; //estas son las banderas del microprocesador las posiciones indican la bandera
+                                        //[0] es N es 1 si el resultado es negatico
+                                        //[1] es Z es 1 si el resultado es cero
+                                        //[2] es C es 1 si hay acarreo en la operacion aritmetica
+                                        //[3] es V es 1 si hubo sobreflujo es la operacion
+    mostrar_registros(Reg);
+    mostrar_banderas(banderas);
+    getch();	/* Espera entrada del usuario */
+	clear();
+
+	Reg[0]=15;
+    Reg[1]=-15;
+    ADD(&Reg[2], Reg[0],Reg[1],&banderas[0]);
+    mostrar_registros(Reg);
+    mostrar_banderas(banderas);
+	getch();
+	clear();
+
+	Reg[0]=15;
+    Reg[1]=-15;
+    SUB(&Reg[2], Reg[0],Reg[1],&banderas[0]);
+    mostrar_registros(Reg);
+    mostrar_banderas(banderas);
+	getch();
+	clear();
+
+
+	endwin();	// Finaliza el modo curses
+
+    return 0;
+}
